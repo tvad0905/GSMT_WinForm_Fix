c423f6e995f11062b6374f85dda3d45a78ba5880
tvad0905 <63111801+tvad0905@users.noreply.github.com>
2020-08-20 17:12:02 +0700

    tách class cho nhưng chưa xong

---
+++ FileExportScheduler/Controller/ExportFileCSV.cs
@@ -0,0 +1,42 @@
+﻿using FileExportScheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExportScheduler.Controller
+{
+    public static class ExportFileCSV
+    {
+        /// <summary>
+        /// Sinh ra file csv trong Export Data folder
+        /// </summary>
+        /// <param name="filePath">danh sách đường dẫn file csv</param>
+        /// <param name="dsThietBi">danh sách thiết bị</param>
+        /// <param name="dsDiemDo">danh sách điểm đo</param>
+        public static void WriteDataToFileCSV(List<string> filePath, Dictionary<string, DeviceModel> dsThietBi, Dictionary<String, List<DataModel>> dsDiemDo)
+        {
+            int i = 0;
+            foreach (KeyValuePair<string, DeviceModel> deviceUnit in dsThietBi)
+            {
+                foreach (KeyValuePair<String, List<DataModel>> duLieuUnit in dsDiemDo)
+                {
+                    string csvData = "[Data]" + "\n" + "Tagname,TimeStamp,Value,DataQuality" + "\n";
+                    foreach (DataModel dt in duLieuUnit.Value)
+                    {
+                        csvData +=
+                                   duLieuUnit.Key + "." + dt.Ten + "," +
+                                   dt.ThoiGianDocGiuLieu.ToString("mm:ss.fff") + "," +
+                                   Math.Round((Convert.ToDouble(dt.GiaTri) / Convert.ToDouble(dt.Scale)), 2) + "," +
+                                   deviceUnit.Value.TrangThaiKetNoi + "\n";
+                    }
+                    File.WriteAllText(filePath[i], csvData);
+                    i++;
+                }
+            }
+
+        }
+    }
+}
